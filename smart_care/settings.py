8566300f3c933fe83bd60d16fecd1d2c949e175b
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-13 15:47:46 +0600

    requirments add

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(ww60h8xqp8yo#p2=+))%i616n%5m(sspqnfj6eh9p@jc9*_a!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
